708448772a448a4d8eabb3ed2b9a2648dab351cb
Joshua Carey <jdcarey128@gmail.com>
2020-09-10 09:21:09 -0600

    Add exists and attributes tests for museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,4 +5,18 @@ require './lib/patron'
 require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
-end 
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+  end
+
+end
