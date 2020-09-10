a3daf3f5857cd1a5fb92101d121f0eaeba27e547
Joshua Carey <jdcarey128@gmail.com>
2020-09-10 09:16:56 -0600

    Add add_patron_interest test and method

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -17,4 +17,12 @@ class PatronTest < Minitest::Test
     assert_equal 20, @patron_1.spending_money
     assert_equal [], @patron_1.interests
   end
+
+  def test_it_can_add_patron_interests
+    assert_equal [], @patron_1.interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    expected = ["Dead Sea Scrolls", "Gems and Minerals"]
+    assert_equal expected, @patron_1.interests
+  end
 end
