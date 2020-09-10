655cf1dd9b94c7674525eaa90d615db0ac5b346f
Joshua Carey <jdcarey128@gmail.com>
2020-09-10 10:51:14 -0600

    Add tests for attending exhibits and spending money

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -158,4 +158,46 @@ class MuseumTest < Minitest::Test
     assert_equal expected, @dmns.announce_lottery_winner(imax)
   end
 
+  def test_it_can_subtract_money_from_patron_after_attending_exhibit
+    skip
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+    tj = Patron.new("TJ", 7)
+    patron_1 = Patron.new("Bob", 10)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("IMAX")
+    @dmns.admit(patron_1)
+    assert_equal 7, tj.spending_money
+    assert_equal 10, patron_1.spending_money
+  end
+
+  def test_patrons_attend_exhibits_they_are_interested_in_and_can_afford
+    skip 
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+    tj = Patron.new("TJ", 7)
+    patron_1 = Patron.new("Bob", 10)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("IMAX")
+    @dmns.admit(patron_1)
+    assert_equal [], tj.exhibits_attended
+    assert_equal [dead_sea_scrolls], patron1.exhibit_interests
+  end
+
+
+
 end
