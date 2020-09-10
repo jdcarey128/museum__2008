da02a46dd5ef66cf34c1c8ddfa41027e40996952
Joshua Carey <jdcarey128@gmail.com>
2020-09-10 10:20:26 -0600

    Add lottery contestant test and method

--- lib/museum.rb
+++ lib/museum.rb
@@ -48,4 +48,10 @@ class Museum
     new_patron_exhibit_interests
   end
 
+  def ticket_lottery_contestants(exhibit)
+    patrons_by_exhibit_interest[exhibit].select do |patron|
+      patron.spending_money < exhibit.cost
+    end
+  end
+
 end
